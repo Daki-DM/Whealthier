ebcf9c687a2fce6797c99d1a36e510b576e47751
codingwith3dv <deveshvdddas@gmail.com>
2021-09-05 13:46:21 +0530

    :sparkle: Add diet-planner view

---
+++ src/views/diet-planner/diet-planner.js
@@ -0,0 +1,21 @@
+import BaseView from '../baseView.js';
+import {
+  DietPlannerComponent
+} from '../../components/diet-planner-component/diet-planner-component.js';
+
+class DietPlanner extends BaseView {
+  constructor() {
+    super();
+    this.setTitle('Name - Diet Planner');
+  }
+  async getHtml() {
+    let htmlString = `
+    <diet-planner-component/>
+    `;
+    return htmlString;
+  }
+};
+
+export {
+  DietPlanner
+};
